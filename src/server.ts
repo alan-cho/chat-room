b83a19f53696a64c37b25e262dcce4c45e929e4b
Alan Cho <alancho397@gmail.com>
2024-11-18 10:10:06 -0800

    added websockets

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,14 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import { createServer } from "http";
+import { Server } from "socket.io";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
 const PORT = 3000;
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const publicDir = path.resolve(__dirname, "../public");
@@ -14,6 +19,10 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(publicDir, "index.html"));
 });
 
-app.listen(PORT, () => {
+io.on("connection", (socket) => {
+  console.log("A user connected");
+});
+
+server.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
