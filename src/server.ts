19901d1174b990bfc84eb39b044c530b7d427d6b
Alan Cho <alancho397@gmail.com>
2024-11-18 10:50:54 -0800

    added ability to listen to messages

--- src/server.ts
+++ src/server.ts
@@ -21,6 +21,12 @@ app.get("/", (req, res) => {
 
 io.on("connection", (socket) => {
   console.log("A user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+  socket.on("chat message", (msg) => {
+    console.log("message: " + msg);
+  });
 });
 
 server.listen(PORT, () => {
